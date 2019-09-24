a862567f422950197eaf365427217432100ddf32
Evandro  Abreu de Oliveira <evandro@MacBook-Pro-de-Evandro.local>
2019-09-23 23:01:04 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
 
 import history from './services/history';
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+// Import Layouts
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+
 export default function RouterWrapper({
   component: Component,
   isPrivate = false,
@@ -14,7 +18,19 @@ export default function RouterWrapper({
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
-  return <Route {...rest} component={Component} />;
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouterWrapper.propTypes = {
