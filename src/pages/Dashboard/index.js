8b048c0529b68bc1bd09f3c932102a7d2aa774c4
Evandro  Abreu de Oliveira <evandro@MacBook-Pro-de-Evandro.local>
2019-10-05 22:50:02 -0300

    final adjusts

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
-// import { Container } from './styles';
+import pt from 'date-fns/locale/pt';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
+
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [schedule, setSchedule] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+      setSchedule(data);
+    }
+    loadSchedule();
+  }, [date]);
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { local: pt }),
+    [date]
+  );
+
+  function hanblePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function hanbleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={hanblePrevDay}>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={hanbleNextDay}>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import AvatarInput from './AvatarInput';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input name="password" type="OldPassword" placeholder="Sua senha" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="password"
+          type="confirmpassword"
+          placeholder="Confirme nova senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
